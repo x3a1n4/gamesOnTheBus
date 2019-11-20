4ce945ca026d166028ff12b3d784093458f6a262
x3a1n4 <billybobbyjoeyyay@gmail.com>
2019-11-20 16:17:09 -0500

    Made buttons go to layouts, and added button that brings you back to the main menu. Next is to maybe look at actually hosting rooms

--- app/src/main/java/com/example/gamesonthebus/MainActivity.java
+++ app/src/main/java/com/example/gamesonthebus/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.gamesonthebus;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,36 +10,84 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ViewFlipper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    List<String> rooms;
+    List<Integer> views;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        views = new ArrayList<>();
+        views.add(R.id.id1);
+        views.add(R.id.id2);
+
+        Button hostGameButton = (Button) findViewById(R.id.hostGameButton);
+        hostGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hostGameClicked();
+            }
+        });
+
+        Button joinGameButton = (Button) findViewById(R.id.joinGameButton);
+        joinGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                joinGameClicked();
+            }
+        });
+
+
+        FloatingActionButton home = findViewById(R.id.fab);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setVisibleView(R.id.id1);
+            }
+        });
+    }
+
+    private void hostGameClicked(){
+        final LinearLayout hostGamePage = (LinearLayout) getLayoutInflater().inflate(R.layout.host_game_page, null);
+
+        final DialogInterface.OnClickListener onClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                try {
+                    rooms.add(findViewById(R.id.gameName).toString());
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+
+            }
+        };
+
+        new AlertDialog.Builder(MainActivity.this)
+                .setView(hostGamePage)
+                .setPositiveButton("Ok", onClickListener)
+                .setNegativeButton("Cancel", null)
+                .show();
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void joinGameClicked(){
+        setVisibleView(R.id.id2);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void setVisibleView(int id){
+        for(Integer i : views){
+            findViewById(i).setVisibility(View.GONE);
         }
-
-        return super.onOptionsItemSelected(item);
+        findViewById(id).setVisibility(View.VISIBLE);
     }
 }
