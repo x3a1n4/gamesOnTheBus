1cb3545a51e6e1998d848dca8b031efc5b36c7b9
x3a1n4 <billybobbyjoeyyay@gmail.com>
2019-12-16 19:53:12 -0500

    Added extra pages, need to make them actually do things

---
+++ app/src/main/java/com/example/gamesonthebus/JoinGamePage.java
@@ -0,0 +1,23 @@
+package com.example.gamesonthebus;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class JoinGamePage extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflator, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflator.inflate(R.layout.game_waiting_page, container, false);
+
+        return view;
+    }
+}
